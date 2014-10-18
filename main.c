6a9df44c03c6a96af950aca284fb911cf24fc168
pekoe.chen <pekoechen@gmail.com>
2014-10-19 01:18:55 +0800

    [Russell]     add feature, support input check

--- main.c
+++ main.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>	// for isdigit()
+#include <string.h>	//for strlen()
+
+
+#define SIZE_BUFFER 256
 
 int main(void) {
 	int n = 0, i, j, temp;
 	char h; 
-
-	while(1)
-	{
+	char str[SIZE_BUFFER] = {'\0'};
+	int length;	
+	
+	
+	while(1){
 		printf("input a integer N between 2 to 9:\n");
-		scanf("%d", &n); 
+		scanf("%s", &str);
+//		printf("str: %s\n", str);
+//		printf("str lenght: %d\n", strlen(str));
+
+	/* check input range */
+		length = strlen(str);
+		for(i=0;i<length;i++){
+			if(!isdigit(str[i]))
+				break;
+		}
+
+		if(i!=length){
+			printf("[ERROR] input is invalidate \n");
+			continue;
+		}	
+
+		/* change data type from "char" to "int" */
+		n = atoi(str);
+//		printf("n: %d\n", n);
+	/////////////////////////////
+	 
+	 
 		if(n>9 || n==1) {
 			puts("只能輸入2到9.眼睛瞎了還是看不懂英文?"); 
 		} 
